b3ed48a2d60da79ba2d1881f08ce6995b0185984
Enrique Del valle <kike@Enriques-MacBook-Pro.local>
2021-08-21 22:42:02 -0500

    first commit

---
+++ src/main/java/com/example/developer/simpleapp/ExampleMavenAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.developer.simpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleMavenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleMavenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/developer/simpleapp/ExampleMavenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.developer.simpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMavenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
